ec94b27ded072acd5a9e4d8cb53dab3ddc743544
DEVENDRAP7 <devendranprajapati07@gmail.com>
2026-04-01 22:05:25 +0530

    Bugs fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.devendrap7.phonemoodtranslator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "2.1.2"
+        versionCode = 10
+        versionName = "2.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
